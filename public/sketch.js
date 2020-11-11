d870816bc1a1ee0228e45cd90a2eddf35eda66e6
mikima <mikimauri@gmail.com>
2020-11-11 12:17:37 +0100

    added socket io both to server and client

---
+++ public/sketch.js
@@ -0,0 +1,32 @@
+let socket = io();
+
+socket.on("connect", newConnection);
+
+function newConnection() {
+  console.log("your id: " + socket.id);
+}
+
+function preload() {
+  // put preload code here
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // put setup code here
+  background("purple");
+}
+
+function draw() {
+  // put drawing code here
+}
+
+function mouseMoved() {
+  ellipse(mouseX, mouseY, 20);
+  // create the message
+  let message = {
+    x: mouseX,
+    y: mouseY,
+  };
+  // send to the server
+  socket.emit("mouse", message);
+}
--- server.js
+++ server.js
@@ -1,1 +1,27 @@
 console.log("node is running");
+
+let express = require("express");
+
+let socket = require("socket.io");
+
+let app = express();
+
+let port = 3000;
+
+let server = app.listen(port);
+
+app.use(express.static("public"));
+
+let io = socket(server);
+
+io.on("connection", newConnection);
+
+function newConnection(socket) {
+  console.log("new connection: " + socket.client.id);
+
+  socket.on("mouse", mouseMessage);
+
+  function mouseMessage(dataReceived) {
+    console.log(dataReceived);
+  }
+}
