51c4f9428db4adad99ec21a8397af0c257ba94f1
mikima <mikimauri@gmail.com>
2020-11-11 12:49:45 +0100

    added broadcast

--- public/sketch.js
+++ public/sketch.js
@@ -1,9 +1,15 @@
 let socket = io();
 
 socket.on("connect", newConnection);
+socket.on("mouseBroadcast", drawOtherMouse);
 
 function newConnection() {
   console.log("your id: " + socket.id);
+}
+
+function drawOtherMouse(data) {
+  fill("yellow");
+  ellipse(data.x, data.y, 20);
 }
 
 function preload() {
@@ -21,6 +27,7 @@ function draw() {
 }
 
 function mouseMoved() {
+  fill("white");
   ellipse(mouseX, mouseY, 20);
   // create the message
   let message = {
